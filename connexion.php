0a5d5ae6b4a6ea551997ab6f3085e7a84c8f17b0
mathieuradjouki <m.radjouki@codeur.online>
2020-09-02 13:48:46 +0200

    commit

--- connexion.php
+++ connexion.php
@@ -1,9 +1,10 @@
-
-<form>
+<?php include('header.php'); ?>
+<div class="container">
+<form class="connexion-form">
   <div class="mb-3">
     <label for="email" class="form-label">Entrez votre email</label>
     <input type="email" class="form-control" id="email" aria-describedby="emailHelp">
-    <div id="email" class="form-text">We'll never share your email with anyone else.</div>
+    <!-- <div id="email" class="form-text">We'll never share your email with anyone else.</div> -->
   </div>
   <div class="mb-3">
     <label for="password" class="form-label">saisissez votre mot de passe</label>
@@ -11,3 +12,5 @@
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
+</div>
+<?php include('footer.php'); ?>
--- subscribe.php
+++ subscribe.php
@@ -1,16 +1,21 @@
-<form>
+<?php include('header.php'); ?>
+
+<div class="container">
+<form class="subscribe-form">
   <div class="mb-3">
-    <label for="email" class="form-label">Entrez votre email</label>
-    <input type="email" class="form-control" id="email">
-    <div id="email" class="form-text">We'll never share your email with anyone else.</div>
+    <label for="email" class="form-label">Votre email</label>
+    <input type="email" class="form-control" id="email" required>
+    <!-- <div id="email" class="form-text">We'll never share your email with anyone else.</div> -->
   </div>
   <div class="mb-3">
-    <label for="password" class="form-label">saisissez votre mot de passe</label>
-    <input type="password" class="form-control" id="password">
+    <label for="password" class="form-label">Votre mot de passe</label>
+    <input type="password" class="form-control" id="password" required>
   </div>
   <div class="mb-3">
-    <label for="password" class="form-label">saisissez à nouveau votre mot de passe</label>
-    <input type="password" class="form-control" id="password">
+    <label for="password-verif" class="form-label">Confirmez votre mot de passe</label>
+    <input type="password" class="form-control" id="password-verif" required>
   </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="submit" class="btn btn-primary" disabled id="submit">Submit</button>
 </form>
+</div>
+<?php include('footer.php'); ?>
